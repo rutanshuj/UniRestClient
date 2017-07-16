7b22986dceb5bc7e8f57af27d3c0cc25c8c32ede
rutanshuj <rjhaveri41@gmail.com>
2017-07-16 23:40:12 +0530

    ImageRetreival

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include_once('simple_html_dom.php');
+
+$base = "https://vtop.vit.ac.in/student/stud_login.asp";
+$html_base = file_get_html($base);
+$url = file_get_contents("https://vtop.vit.ac.in/student/stud_login.asp");
+print_r($url);
+exit;
+if(!empty($html_base)) {
+    foreach ($html_base->find("img") as $element) {
+        echo $element->src. '<br>';
+    }
+}else{
+    echo "Not getting HTML Info";
+}
+    ?>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script type="text/javascript" src="./js/index.js"></script>
---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function (response) {
+    var a = document.getElementsByName("vrfcd");
+    if(document.getElementsByName("vrfcd")!== null){
+        console.log(a);
+        alert("Something happened");
+    }
+
+    Death
+    //var b = a.src;
+
+});
+
+
