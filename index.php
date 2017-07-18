1ae6190d4b146e1c867d8bfb4544c578f3bdad9a
rutanshuj <rjhaveri41@gmail.com>
2017-07-18 21:53:12 +0530

    Image Recog Stuff

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ include_once('simple_html_dom.php');
 $base = "https://vtop.vit.ac.in/student/stud_login.asp";
 $html_base = file_get_html($base);
 $url = file_get_contents("https://vtop.vit.ac.in/student/stud_login.asp");
-print_r($url);
-exit;
 if(!empty($html_base)) {
     foreach ($html_base->find("img") as $element) {
         echo $element->src. '<br>';
@@ -15,4 +13,4 @@ if(!empty($html_base)) {
 }
     ?>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-<script type="text/javascript" src="./js/index.js"></script>
+<script type="text/javascript" src="./js/server.js"></script>
